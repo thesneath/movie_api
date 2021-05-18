072fcbb10e5874e9eedd050a53ac050c7f92194d
thesneath <71525393+thesneath@users.noreply.github.com>
2021-05-18 10:43:09 -0700

    reinstated authentication for get movies

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to my App!');
 });
 
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt',{ session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.json(movies);
